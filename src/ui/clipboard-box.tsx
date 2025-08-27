95f0d5a6605411903005019ea3424dfbcc3b2351
Markus Recaplaza <markusrecaplaza@Markuss-MacBook-Air.local>
2025-08-27 09:53:25 +0800

    fix: removed blurring of background when copying text using hover

--- src/ui/clipboard-box.tsx
+++ src/ui/clipboard-box.tsx
@@ -9,20 +9,31 @@ export default function ClipboardBox({
   timestamp,
   onCopy,
 }: CopyContentProps) {
+  const dateObject: Date = new Date(timestamp);
+  const readableDate: string = new Intl.DateTimeFormat("en-US", {
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(dateObject);
   return (
     <div
       onClick={() => onCopy(text)}
-      className="relative bg-white shadow-neumorphic mt-2 rounded-2xl p-5 flex flex-col w-full justify-center items-center cursor-pointer active:scale-95 hover:scale-102 transition-transform group overflow-hidden"
+      className="relative bg-white shadow-neumorphic mt-2 rounded-2xl p-5 flex flex-col w-full justify-center items-center cursor-pointer active:scale-95 hover:scale-98 transition-transform group overflow-hidden"
     >
+      {/* Date on top left */}
+      <div className="absolute top-3 left-3 z-20 transition-all  text-gray-600 text-xs align-top pb-3">
+        {readableDate}
+      </div>
+
       {/* Text content that will blur on hover */}
-      <div className="text-black text-sm flex items-center z-10 transition-all group-hover:blur-xs group-hover:opacity-40">
-        {text}
+      <div className="flex flex-col z-10 transition-all pt-6">
+        <div className="text-black text-sm items-center">{text}</div>
       </div>
 
       {/* Overlay that shows on hover */}
-      <div className="absolute inset-0 flex items-center justify-center rounded-2xl bg-black/50 text-white text-sm opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
-        Copy Text
-      </div>
+      <div className="absolute inset-0 flex items-center justify-center rounded-2xl bg-gray-300 opacity-0 group-hover:opacity-50 transition-opacity pointer-events-none"></div>
     </div>
   );
 }
